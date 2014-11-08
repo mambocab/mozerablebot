53589202c2b25afeb35b51add041fdc9fbec4bc8
Jim Witschey <jim.witschey@gmail.com>
2014-11-08 14:24:47 -0500

    test tweeting randomly

--- .openshift/cron/minutely/tweet.sh
+++ .openshift/cron/minutely/tweet.sh
@@ -1,2 +1,8 @@
-source ${OPENSHIFT_PYTHON_DIR}virtenv/venv/bin/activate
-python ${OPENSHIFT_REPO_DIR}tweet.py
+if [ $[$RANDOM % 1440] -lt 720 ] ; then
+    echo 'tweeting'
+    source ${OPENSHIFT_PYTHON_DIR}virtenv/venv/bin/activate
+    python ${OPENSHIFT_REPO_DIR}tweet.py
+else
+    echo 'not tweeting this time'
+fi
+
