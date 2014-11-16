3c300c7f75da5ea20385fcd488d999a9212b109f
Jim Witschey <jim.witschey@gmail.com>
2014-11-16 16:21:40 -0500

    pulls out helper functions in tweeter tests hopefully creates a single point of failure in case Tweepy API changes

--- test/test_tweeter.py
+++ test/test_tweeter.py
@@ -3,19 +3,53 @@ from tweeter import Tweeter
 from badwords import ContainsBadWordException
 from unittest.mock import Mock
 
+
 @fixture
 def mock_client():
     return Mock()
+
+
+def get_client_tweet_method(client):
+    '''
+    Getter for the method that should be called when the Twitter client tries
+    to tweet. Provides a single point of failure when client API changes.
+    Actual client currently uses Tweepy, so the method's name is
+    update_status.
+    '''
+    return client.update_status
+
+
+def assert_tweeted(client, s):
+    '''
+    Asserts that client's method for tweeting things was called with s.
+    Provides a single point of failure for when the client's API changes.
+    Actual client currently uses Tweepy, so the method takes a keyword
+    argument called status
+    '''
+    get_client_tweet_method(client).assert_called_with(status=s)
+
 
 def test_clean_status_is_tweeted(mock_client):
     t = Tweeter(mock_client)
     s = 'this is an ok status!'
     t.tweet(s)
-    mock_client.update_status.assert_called_with(s)
+    assert_tweeted(mock_client, s)
+
 
 def test_bad_status_not_tweeted(mock_client):
     t = Tweeter(mock_client)
     s = 'this status contains the word mustard.'
+    assert not get_client_tweet_method(mock_client).called
+
+
+def test_bad_status_raises_exception(mock_client):
+    t = Tweeter(mock_client)
+    s = 'this status contains the word mustard.'
     with raises(ContainsBadWordException):
         t.tweet(s)
-    assert not mock_client.update_status.called
+
+
+def test_bad_status_not_tweeted_case_insensitive(mock_client):
+    t = Tweeter(mock_client)
+    s = 'this status contains the word sauerkrAut.'
+    assert not get_client_tweet_method(mock_client).called
